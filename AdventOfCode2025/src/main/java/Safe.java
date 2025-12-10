f7eabf6467e6054768925c671f7c890fe4923dec
Edmund Woodfield <edmundwoodfield@gmail.com>
2025-12-10 15:26:45 +0000

    Day 1 Part 2 solved

--- AdventOfCode2025/src/main/java/Safe.java
+++ AdventOfCode2025/src/main/java/Safe.java
@@ -6,17 +6,23 @@ public class Safe {
     public int currentPosition;
     public int zeroCounter;
     public void rotate(char direction, int distance){
-        System.out.println("Current position: " + currentPosition);
+        System.out.println("Current position: " + currentPosition + ". Rotating " + direction + distance);
+        var startingAtZero = this.currentPosition == 0;
         if (direction == 'R'){
                 currentPosition = currentPosition + distance;
                 while (currentPosition > 99){
                     currentPosition -= 100;
+                    if (currentPosition != 0) {zeroCounter ++;}
+                    System.out.println("Zero Count: " + zeroCounter);
             }
         }
         if (direction == 'L'){
                 currentPosition = currentPosition - distance;
                 while (currentPosition < 0){
                     currentPosition += 100;
+                    if (!startingAtZero){zeroCounter ++;}
+                    startingAtZero = false;
+                    System.out.println("Zero Count: " + zeroCounter);
             }
         }
         System.out.println("New position: " + currentPosition);
